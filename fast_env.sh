bdc1f1654c341ec0ad2712152451ee27fe42614d
Seunghyun Shin <55797829+seunghyuns98@users.noreply.github.com>
2025-07-17 18:24:07 +0900

    Update fast_env.sh

--- fast_env.sh
+++ fast_env.sh
@@ -1,4 +1,4 @@
-conda create -n color_grading python=3.8.18
+conda create -n color_grading python=3.8.18 -y
 conda activate color_grading
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
 pip install diffusers==0.21.4
